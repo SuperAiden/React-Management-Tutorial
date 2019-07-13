2233cc7cb1a953a7e75292d8d1d997e3abed3b41
SuperAiden <qpqp10212@gmail.com>
2019-07-14 00:44:41 +1000

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Customer from './components/Customer'
+import CustomerAdd from './components/CustomerAdd'
 import Paper from '@material-ui/core/Paper'
 import './App.css';
 import Table from '@material-ui/core/Table';
@@ -25,15 +26,29 @@ const styles = theme =>({
 })
 
 class App extends Component  {
+  constructor(props){
+    super(props);
+    this.state ={
+      customers: "",
+      completed: 0
+    }
+  }
 
-  state = {
-    customers: "",
-    completed: 0
+  stateRefresh = () => {
+    this.setState({
+      customers: "",
+      completed: 0
+    });
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
   }
 
   componentDidMount(){
     this.timer = setInterval(this.progress, 20);
-    this.callApi().then(res => this.setState({customers: res})).catch(err => console.log(err));
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
   }
 
   callApi = async () => {
@@ -46,12 +61,18 @@ class App extends Component  {
   progress = () => {
     const { completed } = this.state;
     this.setState({ completed: completed >= 100 ? 0 : completed + 1});
+    console.log("프로그래스 ", this.customers)
+    
   }
 
   render(){
+    if(this.state.customers){
+      clearInterval(this.timer)
+    }
     const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
+      <div>
+        <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
@@ -66,27 +87,28 @@ class App extends Component  {
           <TableBody>
           {this.state.customers ? this.state.customers.map(c =>{
               return (
-                <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                 birthday={c.birthday}
-                 gender={c.gender}
-                 job={c.job}
-                />
-              )
-            }) : 
-            <TableRow>
-              <TableCell colSpan="6" align="center">
-                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-              </TableCell>
-            </TableRow>
-            }
-          </TableBody>
-        </Table>
-      </Paper>
-     
+                 <Customer
+                 key={c.id}
+                 id={c.id}
+                 image={c.image}
+                 name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                 />
+               )
+             }) : 
+             <TableRow>
+               <TableCell colSpan="6" align="center">
+                 <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+               </TableCell>
+             </TableRow>
+             }
+           </TableBody>
+         </Table>
+       </Paper>
+       <CustomerAdd stateRefresh={this.stateRefresh}/>      
+      </div>
      )
   }
  
