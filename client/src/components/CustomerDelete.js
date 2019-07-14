c6905c7b11390ced59bcdcf547eb34a512488863
SuperAiden <qpqp10212@gmail.com>
2019-07-14 19:32:02 +1000

    Update UI/UX & Add Customer Search Module

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -4,7 +4,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 class CustomerDelete extends React.Component {
 
     constructor(props) {
